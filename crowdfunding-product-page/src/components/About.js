f00048e6db6936a1e9f538fb6c0d84706fdfcf84
Antwi Isaac <antwisaac7@gmail.com>
2022-02-14 19:27:38 +0000

    inner card

--- crowdfunding-product-page/src/components/About.js
+++ crowdfunding-product-page/src/components/About.js
@@ -1,6 +1,30 @@
 import React from "react";
+import Innercard from "./Innercard";
 
 export default function About() {
+  const pledges = [
+    {
+      title: "Bamboo Stand",
+      pledge: "Pledge $25 or more",
+      desc: "You get an ergonomic stand made of natural bamboo",
+      left: "101",
+      id: "1",
+    },
+    {
+      title: "Black Edition Stand",
+      pledge: "Pledge $75 or more",
+      desc: "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our blacker mameber list. Shipping is included",
+      left: "64",
+      id: "2",
+    },
+    {
+      title: "Mahogany",
+      pledge: "Pledge 200 or more",
+      desc: "You get two special edition mahogany stand, a backer tshirt and a personal thank you",
+      left: "0",
+      id: "1",
+    },
+  ];
   return (
     <div className="card">
       <h1 className="text-2xl mb-10 font-bold">About this project</h1>
@@ -15,6 +39,10 @@ export default function About() {
         extra desk space below your computer to allow notepads, pens and USD
         sticks to be stored under the stand
       </p>
+
+      {pledges.map((pledge) => {
+        return <Innercard pledge={pledge} />;
+      })}
     </div>
   );
 }
